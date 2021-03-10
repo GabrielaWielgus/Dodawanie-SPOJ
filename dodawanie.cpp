9b4f3e9417456c92cdafe5b73af347e482f7485e
Gabriela Wielgus <gabwielgus@gmail.com>
2021-03-10 11:08:29 +0100

    Add new files

---
+++ dodawanie.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+    int l_testow, l_liczb, a;
+    cin >> l_testow;
+    for (int i = 0; i < l_testow; i++)
+    {
+        int suma = 0;
+        cin >> l_liczb;
+        for (int i = 0; i < l_liczb; i++)
+        {
+            cin >> a;
+            suma += a;
+        }
+        cout << suma << endl;
+    }
+
+    return 0;
+}
